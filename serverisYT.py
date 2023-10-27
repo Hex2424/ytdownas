cfb9c4c7aeedb8b6d53e6dbd9536d9f7c57a313b
Markas Vielavicius <maildetectionteam@gmail.com>
2023-10-28 00:14:47 +0300

    Fixed a lot of stuff improved design

--- serverisYT.py
+++ serverisYT.py
@@ -3,49 +3,54 @@ from flask import send_file
 from flask import Flask, render_template
 from flask import request
 from flask import redirect
+from concurrent.futures import ThreadPoolExecutor
 import ytdownas
 import os
+import re
+# executor = ThreadPoolExecutor(max_workers=10)
+debug_log = ""
 
-
-
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='html', static_folder='css')
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/download/<filename>')
-def downloadFile (filename):
-    #For windows you need to use drive name [ex: F:/Example.pdf]
-    filePath = ytdownas.downloadVideo(filename)
-    status = send_file(filePath, as_attachment=True)
-    os.remove(filePath)
-    return status
+@app.route('/get_debug_log')
+def get_debug_log():
+    global debug_log
+    return debug_log
 
 @app.route('/handle_post', methods=['POST'])
 def handle_post():
-  
+    global debug_log
     if request.method == 'POST' and request.form['input'] != "":
-        inputy = request.form['input']
-        if "&&" in inputy:
-            inputy = inputy.split("&&")
+        print(request.form)
+        query = request.form['input']
+        seed = request.form['seed']
+  
+        if seed == "":
+            seed = 1
         else:
-            # inputy = [inputy]
-            status = redirect("/download/inputy", code=302)
-
+            seed = int(seed)
         
-        # print(inputy)
+        debug_log = f"Collecting ingridients : '{query}'"
+        url, title = ytdownas.getVideoUrlTitle(query, seed)
 
-        # # for path in inputy:
-        # #     filePath = ytdownas.downloadVideo(path)
-        # #     status = send_file(filePath, as_attachment=True)
-        # #     os.remove(filePath)
+        debug_log = f"Cooking ingridients : '{query}'"
+        path = ytdownas.downloadVideo(url, title)
+
+        if os.path.exists(path):
+            mp3FileName = f"{title}.mp3"
+            debug_log = f"Cooking recipe..."
+            status = send_file(path, as_attachment=True, download_name=mp3FileName)
+            debug_log = f"Here is your cake..."
+            os.remove(path)
+            return status
         
-        return status
-    else:
-        return render_template('index.html')
+    debug_log = f"Song name can't be empty"
+
+    return redirect("/")
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True) 
-
+    app.run(port=5000,host="192.168.1.186",debug=True) 
 
--- ytdownas.py
+++ ytdownas.py
@@ -1,12 +1,37 @@
 from youtube_search import YoutubeSearch
 import os
+from hashlib import sha256
+import yt_dlp
+
+# Create a yt-dlp instance
+ydl = yt_dlp.YoutubeDL()
 
 # filenames = next(walk("/media/hex24/3863-5EE5"), (None, None, []))[2]  # [] if no file
-def downloadVideo(filename):
-    result = YoutubeSearch(os.path.basename(filename), max_results=1).to_dict()[0]
+def getVideoUrlTitle(query, seed):
+    result = YoutubeSearch(query, max_results=seed).to_dict()[seed - 1]
     title = result['title']
     url = result['url_suffix']
+    return (url, title)
 
-    os.system(f"yt-dlp -x --audio-format mp3 --audio-quality 0 'https://www.youtube.com{url}' -o '{title}'")
+def downloadVideo(url, title):
 
-    return './'+ title + '.mp3'
+    hashedTitle = sha256(title.encode('utf-8')).hexdigest()
+
+    options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',  # Set the preferred codec to mp3
+            'preferredquality': '192'  # Set the preferred audio quality
+        }],
+        'outtmpl' : f'./music/{hashedTitle}'
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        ydl.extract_info(f'https://www.youtube.com{url}', download=True)
+
+    path = './music/'+ hashedTitle + '.mp3'
+
+    return path
+
+# downloadVideo("hello")
